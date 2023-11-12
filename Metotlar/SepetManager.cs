059173da5285474f55f3bcf41d95f4d4a7b28c32
KUBRA YAVUZ <128039616+kubra-yavuz@users.noreply.github.com>
2023-11-13 01:46:26 +0300

    Metotlar

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Metotlar
+{
+	public class SepetManager
+	{
+
+		public void Ekle(Urun urun)
+		{
+			Console.WriteLine("Tebrikler! Sepete eklendi : " + urun.Adi);
+		}
+
+		
+	}
+}
+
